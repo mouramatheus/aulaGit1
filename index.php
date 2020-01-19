1e176160dee3dbaf91e59ca492154ad2fe6b5000
mouramatheus <matheusmfonseca1@gmail.com>
2020-01-19 09:35:54 -0300

    TEste novo commit

--- index.php
+++ index.php
@@ -11,6 +11,27 @@
         git diff -> lista todas as modificações de maneira detalhada
 
         git commit -m "descricao do commit" 
+
+
+        Novos comandos:
+        git commit -am "descricao do commit" ->faz o commit ate de arquivos nao track
+        
+
+        Voltando a log antigos:
+        git reset --hard codigoDoCommit
+
+        Conectando ao respositorio remoto:
+        git remote -v ->verifica aonde esta conectado
+
+        git remote rm origin -> remove a conexao com o repositorio
+
+        conectando ao repositorio:
+        git remote add origin urlDoRepositorio
+
+        Como puxar do repositorio virtual para local para os dois
+        estarem em sincronia ?
+
+        git pull origin master
     </body>
 
 </html>
