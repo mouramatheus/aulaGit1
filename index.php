1c8a3eaf46a725dc657fd8449e27a8d5cea573be
mouramatheus <matheusmfonseca1@gmail.com>
2020-01-19 08:54:40 -0300

    Novo commit teste

--- index.php
+++ index.php
@@ -5,6 +5,12 @@
 
     <body>
         git init -> inicializa repositorio
+        git config --global user.name "Matheus Moura" -> Coloca o nome no projeto
+        git add "nomeDoArquivo" ou para todos git add * -> adiciona os arquivos para serem monitorados pelo git (track)
+        git add -A -> adiciona tudo
+        git diff -> lista todas as modificações de maneira detalhada
+
+        git commit -m "descricao do commit" 
     </body>
 
 </html>
